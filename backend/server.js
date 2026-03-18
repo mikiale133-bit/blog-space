cfac1f9eab9d0cd592a35bad6e2ccea70c677eb2
mikiale133-bit <mikiale133@gmail.com>
2026-03-18 20:26:11 +0300

    mm

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,6 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// handle preflight explicitly
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
